e6ba72bacf654554dcdd56b2872fcb3e42d7fbf6
Mehdi Zeinali <mehdi.zeinali@auperatech.com>
2024-03-05 16:59:13 -0800

    Update aup_vmss_setup.sh
    add -y to apt command

--- setup/K260_Kria_SOM/aup_vmss_setup.sh
+++ setup/K260_Kria_SOM/aup_vmss_setup.sh
@@ -79,7 +79,7 @@ if ! type docker &>/dev/null; then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 else
     echo "Docker is already installed."
 fi
